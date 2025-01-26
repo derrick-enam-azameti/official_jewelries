4b12ab23b7f622172a3d4a387f80d9c2e19a8c0e
Derrick Enam Azameti <azametid@gmail.com>
2025-01-26 22:49:30 +0000

    Add: Base App

---
+++ README.md
@@ -0,0 +1,4 @@
+# Official Jewelries
+Official Jewelries is a webapp that helps customers to search for beautiful jewelries and make purchases.
+
+It is developed using nextjs and redux. It also employs the use of some apis.
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import CategoryCard from '../components/CategoryCard';
+import styles from '../styles/Home.module.css';
+
+const HomePage = () => {
+  return (
+    <main className={styles.container}>
+      <div className={styles.small}>
+        <CategoryCard image="https://imgur.com/rX6g0yJ.jpg" name="RINGS" />
+        <CategoryCard image="https://imgur.com/SMqvLOM.png" name="NECKLACES" />
+        <CategoryCard image="https://imgur.com/IQgwfxS.jpg" name="EARRINGS" />
+      </div>
+      <div className={styles.large}>
+        <CategoryCard image="https://imgur.com/n6E3tz6.jpg" name="WATCHES" />
+        <CategoryCard image="https://imgur.com/ujySRXV.jpg" name="GEMSTONES" />
+      </div>
+    </main>
+  );
+};
+
+export default HomePage;
